823683d4a1ed128b971c257eda5c4c41a3462397
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-09-09 17:54:28 +0300

    update

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/romeo')
+def romep():
+    return render_template('romeo_and_julet.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
