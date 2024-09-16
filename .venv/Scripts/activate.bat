3f1a2b778a241300da032f51c284b7e2c5b26695
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-09-16 15:27:56 +0300

    update

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Darin\Documents\GitHub\online_library\.venv
+set VIRTUAL_ENV=c:\Users\Darin_btw\Documents\GitHub\online_library\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
