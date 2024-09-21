25a97d3e99507a785da3bcbda7531faa39052388
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-09-21 14:08:37 +0300

    update search

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+db = sqlite3.connect('books.db')
+cur = db.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS name_books(
+    name TEXT,
+    authors TEXT 
+    )
+    ''')
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/books')
+def books():
+    db = sqlite3.connect()
+    cur = db.cursor()
+    cur.execute('SELECT * FROM name_books')
+    books_data = cur.fetchall()
+    db.close()
+    return render_template('books.html', books=books_data)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+    db = sqlite3.connect('books.db')
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM name_books
+                WHERE name LIKE ? OR authors LIKE ?
+                ''', ('%' + query + '%', '%' + query + '%'))
+    search_result = cur.fetchall()
+    db.close()
+    return render_template('books.html', query=query, books=search_result)
 
 @app.route('/romeo')
 def romep():
